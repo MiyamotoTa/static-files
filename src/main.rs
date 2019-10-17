840302e1606da0834dd357d181919549ed33508b
Takahiro Miyamoto <takahiro.miyamoto@glv.co.jp>
2019-10-17 17:03:11 +0900

    静的ファイルを返す

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use actix_web::{fs, server, App};
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| App::new().handler("/", fs::StaticFiles::new(".").unwrap()))
+        .bind("127.0.0.1:3000")
+        .expect("Can not bind to port 3000")
+        .run();
 }
